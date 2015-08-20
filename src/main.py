de32f8efc098cb9838171b821f281f4541966210
danielstarman <dstarmanucf@gmail.com>
2015-08-20 08:33:00 -0400

    Added in main, which wasn't included automatically for some reason.

---
+++ src/main.py
@@ -0,0 +1,21 @@
+﻿"""
+    The main module.
+"""
+import sys
+from PySide import QtGui, QtCore
+from fitnessmodel import FitnessModel
+from fitnesscontroller import FitnessController
+from fitnessview import FitnessView
+
+class App(QtGui.QApplication):
+
+    def __init__(self, sys_argv):
+        super(App, self).__init__(sys_argv)
+        self.model = FitnessModel()
+        self.view = FitnessView()
+        self.controller = FitnessController(self.model, self.view)
+        self.view.show()
+
+if __name__ == '__main__':
+    app = App(sys.argv)
+    sys.exit(app.exec_())
